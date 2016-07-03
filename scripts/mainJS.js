e8876df608e84c0c500618b7cdf1b8801d882e44
YOUR NAME <dragomir.tachev@gmail.com>
2016-07-03 03:32:43 +0300

    started some animation....

--- scripts/mainJS.js
+++ scripts/mainJS.js
@@ -14,21 +14,29 @@ function ResizeSVG() {
 function InitializeComponents() {
   var svg = d3.select('svg');
 
-  var topLeft = svg.append('path')
+  var topLeft = svg.append('g').append('path')
   .attr('d', 'M0,40 Q40,115 80,40 Q40,-35 0,40 M80,40 Q130,110 70,170 Q90,90 55,70Z')
   .attr('fill','lime')
   .attr('stroke', 'lime')
   .attr('stroke-width', '5');
 
-  var topRight = svg.append('path')
+  var topRight = svg.append('g').append('path')
   .attr('d', 'M0,40 Q40,115 80,40 Q40,-35 0,40 M80,40 Q130,110 70,170 Q90,90 55,70Z')
   .attr('fill', 'lime')
   .attr('stroke', 'lime')
   .attr('stroke-width', '5')
   .attr('transform', 'translate(695,0)');
 
-  d3.selectAll('path')
+  d3.selectAll('g')
   .transition()
-  .duration(2000)
-  .attr('transform','translate(350,230)')
+        .duration(2000)
+        .attr('transform','translate(260,340)')
+        .attrTween("transform", function () {
+          var i = d3.interpolate(0, 270);
+          return function (t) {
+            return 'rotate(' + i(t) + ',' + '250,350)';
+          }
+          
+  })
+   
 }
